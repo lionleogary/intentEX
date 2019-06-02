a1e1db348b0015636bb5f227ca4029979d25133a
leo <lionleogary@gms.tku.edu.tw>
2019-06-02 15:45:44 +0800

    uu

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,6 +35,35 @@ public class ResultActivity extends AppCompatActivity{
                 "\nsum="+sum+
                 "\naverage="+nf.format(average);
         tvResult.setText(text);
+        alert(average);
+
+
+    }
+    private void alert(double average){
+        String message=new String();
+        String tittle=new String();
+        int pic=0;
+        if(average==100){
+            message="100";
+            tittle="pass";
+            pic=R.drawable.circle;
+        }
+        else if(average>=60){
+            message="congratulation";
+            tittle="pass";
+            pic=R.drawable.circle;
+        }
+        else {
+            message="fail";
+            tittle="fail";
+            pic=R.drawable.circle;
+        }
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        builder.setMessage(message);
+        builder.setTitle(tittle);
+        builder.setIcon(pic);
+        builder.show();
+
 
     }
     public  void onBackClick(View view){
